a4b327a3b693911da09cc6348418ab74b2b9d62f
Alex <format37@gmail.com>
2025-09-06 21:28:10 +0400

    daemon

--- compose.sh
+++ compose.sh
@@ -2,4 +2,4 @@ sudo docker rm -f mcp-weather-resource-server || true
 sudo docker rm -f mcp-weather-auth-server || true
 # Build
 sudo docker compose build
-sudo docker compose up --force-recreate
+sudo docker compose up --force-recreate -d
